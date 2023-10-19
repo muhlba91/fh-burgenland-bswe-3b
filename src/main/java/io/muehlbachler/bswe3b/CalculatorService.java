8037249dfb226d12941948301d561b8bea811c6a
Máhli Balázs <mahli@posteo.net>
2023-10-19 18:21:37 +0200

    docs: Added JDoc to CalculatorService and it's functions.

--- src/main/java/io/muehlbachler/bswe3b/CalculatorService.java
+++ src/main/java/io/muehlbachler/bswe3b/CalculatorService.java
@@ -2,12 +2,20 @@ package io.muehlbachler.bswe3b;
 
 import org.springframework.stereotype.Service;
 
-// FIXME: add javadoc
+/**
+ * Represents a calculator service, which provides only division so far.
+ */
 @Service
 public class CalculatorService {
-    // FIXME: add javadoc
+    /**
+     * Divides two int type numbers and returns the ans.
+     *
+     * @param a The dividend.
+     * @param b The divisor.
+     * @return The ans of the division.
+     */
     public int divide(final int a, final int b) {
-        // FIXME: implement
-        return -1;
+        // if only division by zero is the problem, then this should be b == 0
+        return b < 0 ? 0 : a/b;
     }
 }
