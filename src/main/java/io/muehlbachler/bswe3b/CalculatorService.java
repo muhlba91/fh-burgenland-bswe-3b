56e3387775e0ecdfede6f689201dad5c4da90f05
ViktoriaVass <114686289+ViktoriaVass@users.noreply.github.com>
2023-10-20 15:56:17 +0200

    New tests

--- src/main/java/io/muehlbachler/bswe3b/CalculatorService.java
+++ src/main/java/io/muehlbachler/bswe3b/CalculatorService.java
@@ -5,9 +5,14 @@ import org.springframework.stereotype.Service;
 // calculator - documentation n
 @Service
 public class CalculatorService {
-    // divide - should return a/b
-    public int divide(final int a, final int b) {
-        // FIXME: implement
-        return -1;
+   public int divide(final int a, final int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return a / b;
+    }
+
+    public int multiply(final int a, final int b) {
+        return a * b;
     }
 }
--- src/test/java/io/muehlbachler/bswe3b/CalculatorServiceTest.java
+++ src/test/java/io/muehlbachler/bswe3b/CalculatorServiceTest.java
@@ -11,5 +11,13 @@ public class CalculatorServiceTest {
         assertEquals(1, calc.divide(1, 1), "1/1 should be 1");
     }
 
-    // FIXME: implement more tests
+    @Test
+    public void testDividePositiveNumberWithRemainder() {
+        assertEquals(2, calc.divide(5, 2), "5/2 should be 2 with remainder 1");
+    }
+
+    @Test
+    public void testMultiplyPositiveNumbers() {
+        assertEquals(6, calc.multiply(2, 3), "2 * 3 should be 6");
+    }
 }
